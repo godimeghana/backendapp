10e8db32cb9d9743e6e75ef3f6d7d2911212ea70
godimeghana <godimeghana28@gmail.com>
2025-05-27 16:15:20 +0530

    time12

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify,send_from_directory, render_template_string
 from flask_cors import CORS
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from datetime import datetime,timezone
 import psycopg2
 import psycopg2.extras
 import os
@@ -75,7 +74,7 @@ def save_document():
     data = request.json
     name = data.get('name')
     content = data.get('content')
-    current_time = datetime.now(ZoneInfo("Asia/Kolkata"))
+    current_time = datetime.now(timezone.utc)
 
     try:
         conn = get_db_connection()
